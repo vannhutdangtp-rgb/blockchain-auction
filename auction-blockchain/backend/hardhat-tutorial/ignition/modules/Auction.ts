83f7c81baf826130b56e8f58e3cf82dd8ef90d75
Đặng Văn Nhựt <vannhutdangtp@gmail.com>
2026-02-02 13:09:35 +0700

    Delete auction-blockchain directory

--- auction-blockchain/backend/hardhat-tutorial/ignition/modules/Auction.ts
+++
@@ -1,6 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("AuctionModule", (m) => {
-  const counter = m.contract("Auction");
-  return { counter };
-});
